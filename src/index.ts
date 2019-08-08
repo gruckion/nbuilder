ec2de64aaa1a7c5f3cc131ab1350394e67a33b37
Stephen Rayner <stephen.r.rayner@gmail.com>
2019-08-08 13:19:18 +0100

    work in progress, getting build to make a string

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import { Builder } from "./Builder";
+
 console.log("start");
 
+const builder = Builder.createNew<string>();
+const withValue = builder.with("hello");
+const value = withValue.build();
+
+console.log("builder:", builder);
+console.log("with:", withValue);
+console.log("value:", value);
